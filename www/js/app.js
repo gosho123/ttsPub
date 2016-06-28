8ce9ff0da32c075ab21b2f589f25f5e4e7d4a845
Paul Jones <paul@go-sho.com>
2016-06-28 14:41:48 +0100

    3.30

--- www/js/app.js
+++ www/js/app.js
@@ -40,7 +40,7 @@ function onDeviceReady() {
 
     jQuery('#platform').html(thisDevice);
 
-    jQuery('#trace').html('v3.29');
+    jQuery('#trace').html('v3.30');
 
     jQuery('#screen-login').hide();
     jQuery('#screen-login').show();
